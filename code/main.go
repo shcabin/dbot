5a61f4f8b6494cf48b9515e3c4f52ab1c8eb6ebf
shcabin <shcabin@helloDD>
2024-06-25 00:03:36 +0800

    post

--- code/main.go
+++ code/main.go
@@ -6,12 +6,12 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.POST("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.GET("/", func(c *gin.Context) {
+	r.POST("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world",
 		})
